d1fec59a29c1f94b22047ff344699fa011581d3c
Nitish Victor <nithishvictor@gmail.com>
2018-01-27 12:58:04 -0500

    Zombie Hives

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour {
+
+    float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position = transform.position + new Vector3(0.0f, 0.0f, speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = transform.position + new Vector3(0.0f, 0.0f, -speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position + new Vector3(-speed * Time.deltaTime, 0.0f, 0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+        }
+    }
+}
--- Assets/Scripts/FollowLeader.cs
+++ Assets/Scripts/FollowLeader.cs
@@ -1,16 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class FollowLeader : MonoBehaviour {
+public class FollowLeader : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public NavMeshAgent navMesh;
+    public bool isLeader = true;
+    float maxVelocity = 10.0f;
+    float maxForce = 10.0f;
+    float maxSpeed = 10.0f;
+    float mass = 10.0f;
+    Vector3 velocity;
+    Vector3 position;
+    const float SEPARATION_RADIUS = 1.0f;
+    const float MAX_SEPARATION = 10.0f;
+    const float LEADER_BEHIND_DIST = 5.0f;
+    
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        GoToMousePositionIfLeader();
+    }
+
+    void GoToMousePositionIfLeader()
+    {
+        if (Input.GetMouseButtonDown(0) && isLeader)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name == "Plane")
+                {
+                    navMesh.destination = hit.point;
+                }
+            }
+        }
+    }
 }
